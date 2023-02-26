db9d011ff8a31cbd27f63e191dd97e0c0bc2b591
Colin Perepelken <colin@sparelabs.com>
2023-02-25 19:01:38 -0800

    Fix eslint

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Colin&apos;s Routing ApP (CRAP)</h1>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <h1 className="font-bold underline">
+                    🚗 Colin&apos;s Routing ApP (CRAP 💩)
+                </h1>
             </header>
         </div>
     )
