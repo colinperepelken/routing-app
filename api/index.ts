945e11ac54f9ddab79a177c6c8f8a9defd2a8c54
Colin Perepelken <colin@sparelabs.com>
2023-02-25 16:15:32 -0800

    Added routingjs dependency

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,25 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { Valhalla } from '@routingjs/valhalla'
 
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT ?? 3000
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
+const routingServiceBaseUrl = 'http://localhost:8002'
+
+app.get('/', async (req: Request, res: Response) => {
+    const routingClient = new Valhalla({ baseUrl: routingServiceBaseUrl })
+    const directions = await routingClient.directions(
+        [
+            [49.856413, -119.47166],
+            [49.85901360673831, -119.49025777469902],
+        ],
+        'auto',
+    )
+
+    res.json(directions)
 })
 
 app.listen(port, () => {
